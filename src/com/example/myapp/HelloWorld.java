d4019b25ebeae5b772af62ea54ab567e2fa020ae
Amir Hossein Pakdel <Amir.pakdel1378@yahoo.com>
2024-11-22 12:10:33 +0100

    Hi Commit

--- src/com/example/myapp/HelloWorld.java
+++ src/com/example/myapp/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	String myString;
 
 	void shout() {
-		myString = "Hello World!";
+		myString = "Hi World!";
 		for (int i = 0; i < 3; i++) {
 			System.out.println(myString);
 		}
